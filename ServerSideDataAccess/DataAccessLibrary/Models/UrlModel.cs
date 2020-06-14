b21cdf239bbed63db72cde763798ce8b3e15c5fd
alistairdevine <alistair.devine98@gmail.com>
2020-06-14 22:16:46 +0100

    Creation of the Data Access Library
    Setting up the data access library for this example project. Getting the Sql statements and set up the Url data, ready for new a new database external from visual studio but local to the computer.

---
+++ ServerSideDataAccess/DataAccessLibrary/Models/UrlModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    class UrlModel
+    {
+        public string Url { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ ServerSideDataAccess/DataAccessLibrary/UrlData.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class UrlData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UrlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<UrlModel>> GetUrl()
+        {
+            string sql = "select * from dbo.Url";
+
+
+        }
+    }
+}
